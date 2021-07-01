e55c41e49b9ff0c283923db6fee4fd56d8feca01
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-01 11:13:44 +0100

    roles

--- src/main/java/com/example/demo/entity/AppRole.java
+++ src/main/java/com/example/demo/entity/AppRole.java
@@ -1,5 +1,27 @@
 package com.example.demo.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class AppRole {
+@Id
+private String roleName;
+
+public AppRole(String roleName) {
+	this.roleName = roleName;
+}
+
+public AppRole() {
+}
+
+public String getRoleName() {
+	return roleName;
+}
+
+public void setRoleName(String roleName) {
+	this.roleName = roleName;
+}
+
 
 }
--- src/main/java/com/example/demo/entity/AppUser.java
+++ src/main/java/com/example/demo/entity/AppUser.java
@@ -3,13 +3,14 @@ package com.example.demo.entity;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import org.hibernate.annotations.ManyToAny;
 
 import com.sun.istack.NotNull;
-
+@Entity
 public class AppUser {
 @Id
 private String username;
@@ -17,6 +18,39 @@ private String username;
 private String password;
 @NotNull
 private int active;
-@ManyToMany()
+@ManyToMany(fetch= FetchType.EAGER)
 private Collection<AppRole> roles=new ArrayList<>();
+public AppUser() {
+}
+public AppUser(String username, String password, int active, Collection<AppRole> roles) {
+	this.username = username;
+	this.password = password;
+	this.active = active;
+	this.roles = roles;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public int getActive() {
+	return active;
+}
+public void setActive(int active) {
+	this.active = active;
+}
+public Collection<AppRole> getRoles() {
+	return roles;
+}
+public void setRoles(Collection<AppRole> roles) {
+	this.roles = roles;
+}
+
 }
