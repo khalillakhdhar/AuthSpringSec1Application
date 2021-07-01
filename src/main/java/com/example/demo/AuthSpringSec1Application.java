981fd0337b3d489451ebf32459c3d20577e595a4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-01 13:07:54 +0100

    service

--- src/main/java/com/example/demo/AuthSpringSec1Application.java
+++ src/main/java/com/example/demo/AuthSpringSec1Application.java
@@ -6,21 +6,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.example.demo.dao.TaskRepository;
+import com.example.demo.entity.AppRole;
+import com.example.demo.entity.AppUser;
 import com.example.demo.entity.Task;
+import com.example.demo.service.AccountService;
 
 @SpringBootApplication
 public class AuthSpringSec1Application  implements CommandLineRunner {
+	@Autowired
+	private AccountService accountService;
 	@Autowired
 	private TaskRepository taskRepository;
 	@Autowired
 	public static void main(String[] args) {
 		SpringApplication.run(AuthSpringSec1Application.class, args);
 	}
+	@Bean
+	public BCryptPasswordEncoder getBCPE() {
+	return new BCryptPasswordEncoder();
+	}
 	@Override
 	public void run(String... args) throws Exception {
-	// TODO Auto-generated method stub
+	// TODO block d'initialisation 
+		accountService.saveUser(new AppUser("admin", "1234",1,null));
+		accountService.saveUser(new AppUser("user", "1234",1,null));
+		accountService.saveRole(new AppRole("ADMIN"));
+		accountService.saveRole(new AppRole("USER"));
+		accountService.addRoleToUser("admin", "ADMIN");
+		accountService.addRoleToUser("user", "USER");
 	Stream.of("T1","T2","T3").forEach(t->{
 	taskRepository.save(new Task(t));
 	});
--- src/main/java/com/example/demo/service/AccountServiceImpl.java
+++ src/main/java/com/example/demo/service/AccountServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,37 +10,45 @@ import com.example.demo.dao.AppUserRepository;
 import com.example.demo.entity.AppRole;
 import com.example.demo.entity.AppUser;
 
+
 @Service
 @Transactional
 public class AccountServiceImpl implements AccountService {
-	@Autowired
+@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	@Autowired
-	private AppUserRepository userRepository;
-	@Autowired
-	private AppRoleRepository roleRepository;
-	@Override
-	public AppUser saveUser(AppUser user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	   @Autowired
+	   private AppUserRepository userRepository;
+	   @Autowired 
+	   private AppRoleRepository roleRepository;
+		@Override
+		public AppUser saveUser(AppUser user) {
+			// TODO Auto-generated method stub
+			String hashPW=bCryptPasswordEncoder.encode(user.getPassword());
+			
+			user.setPassword(hashPW);
+			
+			return userRepository.save(user);
+		}
 
-	@Override
-	public AppRole saveRole(AppRole role) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		@Override
+		public AppRole saveRole(AppRole role) {
+			// TODO Auto-generated method stub
+			return roleRepository.save(role);
+		}
 
-	@Override
-	public void addRoleToUser(String username, String roleName) {
-		// TODO Auto-generated method stub
+		@Override
+		public void addRoleToUser(String username, String roleName) {
+			// TODO Auto-generated method stub
+			
+			AppRole role=roleRepository.findByRoleName(roleName);
+			AppUser user = userRepository.findByUsername(username);
+			user.getRoles().add(role);
+			
+		}
 
-	}
-
-	@Override
-	public AppUser findUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+		@Override
+		public AppUser findUserByUsername(String username) {
+			// TODO Auto-generated method stub
+			return userRepository.findByUsername(username);
+		}
 }
