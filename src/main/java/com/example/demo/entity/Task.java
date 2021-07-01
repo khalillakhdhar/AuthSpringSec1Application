00aae13098286e13ac5d9db69aedc5868a46fdfd
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-07-01 09:15:21 +0100

    first

---
+++ src/main/java/com/example/demo/entity/Task.java
@@ -0,0 +1,33 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+
+public class Task {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private long id;
+@NotNull
+private String taskName;
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getTaskName() {
+	return taskName;
+}
+public void setTaskName(String taskName) {
+	this.taskName = taskName;
+}
+
+
+
+}
